fbc8670fafb42ac6d05e6fac4cf8353a14934681
David Merchan <juan1014166@gmail.com>
2021-10-27 10:15:36 -0500

    feat(database): Add database configuration

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,5 +1,28 @@
+import { ConfigType } from '@nestjs/config';
 import { Global, Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { getSsl } from '../common/get-ssl';
+import config from '../app.config';
 
 @Global()
-@Module({})
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: (configService: ConfigType<typeof config>) => {
+        const ssl = getSsl();
+
+        return {
+          entities: ['dist/**/*.entity{.ts,.js}'],
+          retryAttempts: 10,
+          retryDelay: 3000,
+          ssl,
+          type: 'postgres',
+          url: configService.databaseUrl,
+        };
+      },
+      inject: [config.KEY],
+    }),
+  ],
+})
 export class DatabaseModule {}
