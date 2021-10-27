5cce7edd3171c3193842c79d2a53dc4767c161a4
David Merchan <juan1014166@gmail.com>
2021-10-27 09:44:49 -0500

    feat(app): Add environment variables configuration
    additionally, the base configurations were added to work in different
    environments

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,21 @@
+import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
+import * as Joi from 'joi';
 
+import { DatabaseModule } from './database/database.module';
+import { enviroments } from './app.enviroments';
+import config from './app.config';
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: enviroments[process.env.NODE_ENV] || '.env',
+      load: [config],
+      isGlobal: true,
+      validationSchema: Joi.object({
+        DATABASE_URL: Joi.string().required(),
+      }),
+    }),
+    DatabaseModule,
+  ],
 })
 export class AppModule {}
